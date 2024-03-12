b10d8c57531b3e549ed49c2cb753bb292b6745e2
cynthiacruz <ccruzgz@gmail.com>
2024-03-12 12:19:30 -0600

    Tested for the mean calculations

--- mean_var_std.py
+++ mean_var_std.py
@@ -2,7 +2,7 @@ import numpy as np
 
 # TODO the input of the func should be a list containing 9 digits
 # TODO the func should return a dict containing mean, variance, standard deviation, 
-#      max, min, and sum along both axes and for the flattened matrix
+#      max, min, and along both axes and for the flattened matrix
 # TODO the values in the returned dictionary should be lists and not Numpy arrays.
 
 def calculate(list):
@@ -22,8 +22,8 @@ def calculate(list):
         # }
         # return calculations
 
-        axis1_sum = np.sum(a)
-        print(axis1_sum)
+        mean_calc = [np.mean(a, axis=0), np.mean(a, axis=1), np.mean(a)]
+        print(mean_calc)
 
 test_list = (0, 1, 2, 3, 4, 5, 6, 7, 8)
 
